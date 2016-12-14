31a86d59ebd7bd8daffb8e6ba3329d5ba6bedd26
Mr_liang <578423625@qq.com>
2016-12-14 14:10:58 +0800

    chang json name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,8 @@
         <div style="text-align: center;display: inline-block;">
             <div style="font-size: 96px;">welcome</div>
         </div>
-        <p style="margin-left:700px;font-size: 40px">laravel5框架</p>
+        <p>周绍天(2014211548),李术(2014211565),李元术(2014211477)</p>
+        {{--<p style="margin-left:700px;font-size: 40px">laravel5框架</p>--}}
 </div>
 
 
